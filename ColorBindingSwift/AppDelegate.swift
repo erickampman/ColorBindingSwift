38cf32445783b82dd0b8cdf120f685fa0d681f53
Eric Kampman <erickampman@yahoo.com>
2016-05-14 07:14:28 -0700

    Initial Commit

---
+++ ColorBindingSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ColorBindingSwift
+//
+//  Created by Eric Kampman on 5/14/16.
+//  Copyright © 2016 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
